ca73b773dbce9ffde4a2980d5ee43db2c1e1eb11
Alexander_Ovchar <azalexxx02@gmail.com>
2024-11-08 13:58:15 +0200

    add nodemon

--- index.js
+++ index.js
@@ -1,23 +1,38 @@
-const http = require("http");
-const fs = require("fs");
+// const http = require("http");
+// const fs = require("fs");
 
-const homePage = fs.readFileSync("index.html");
-const aboutPage = fs.readFileSync("about.html");
-const contactPage = fs.readFileSync("contact.html");
+// const homePage = fs.readFileSync("index.html");
+// const aboutPage = fs.readFileSync("about.html");
+// const contactPage = fs.readFileSync("contact.html");
 
-const server = http.createServer((request, response) => {
-  if (request.url === "/") {
-    response.end(homePage);
-  } else if (request.url === "/about") {
-    response.end(aboutPage);
-  } else if (request.url === "/contact") {
-    response.end("Contact page");
-  } else {
-    response.writeHead(404, {"Content-Type": "text/html"});
-    response.end("Not Found");
-  }
+// const server = http.createServer((request, response) => {
+//   if (request.url === "/") {
+//     response.end(homePage);
+//   } else if (request.url === "/about") {
+//     response.end(aboutPage);
+//   } else if (request.url === "/contact") {
+//     response.end("Contact page");
+//   } else {
+//     response.writeHead(404, {"Content-Type": "text/html"});
+//     response.end("Not Found");
+//   }
+// });
+
+// server.listen(3000, () => {
+//   console.log("Server is running on port 3000");
+// });
+
+const express = require("express");
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.json({
+    message: "Home page",
+  });
+  // res.send("Home page");
 });
 
-server.listen(3000, () => {
+app.listen(3000, () => {
   console.log("Server is running on port 3000");
 });

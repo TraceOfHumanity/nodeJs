0dab4fd5b27b8740daf17ba23853373b04ce5434
Alexander_Ovchar <azalexxx02@gmail.com>
2024-11-08 16:16:16 +0200

    add expressEdge

--- index.js
+++ index.js
@@ -1,11 +1,27 @@
 const express = require("express");
 const app = express();
 const path = require("path");
+const expressEdge = require("express-edge");
 
 app.use(express.static(path.resolve(__dirname, "public")));
+app.use(expressEdge);
+app.set("views", path.resolve(__dirname, "views"));
+
 
 app.get("/", (req, res) => {
   res.sendFile(path.resolve(__dirname, "pages/index.html"));
+});
+
+app.get("/about", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "pages/about.html"));
+});
+
+app.get("/contact", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "pages/contact.html"));
+});
+
+app.get("/post", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "pages/post.html"));
 });
 
 app.listen(4000, () => {

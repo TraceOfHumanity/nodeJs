9f810179dc35f7919a81bcd04205d6e5ae206809
Alexander_Ovchar <azalexxx02@gmail.com>
2024-11-08 13:16:12 +0200

    add routing

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
 const http = require("http");
 
 const server = http.createServer((request, response) => {
-  console.log(request.url);
-  response.end("Hello World");
+  if (request.url === "/") {
+    response.end("Home page");
+  } else if (request.url === "/about") {
+    response.end("About page");
+  } else if (request.url === "/contact") {
+    response.end("Contact page");
+  } else {
+    response.writeHead(404, {"Content-Type": "text/html"});
+    response.end("Not Found");
+  }
 });
 
 server.listen(3000, () => {
